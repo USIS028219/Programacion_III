60071955e90360bd02598812b5dd92234412c291
laura623 <laurriguerrero@gmail.com>
2020-08-30 21:07:18 -0600

    Se agregaron los datos a los Spinners, falta la ecuacion para que haga la conversion

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -48,6 +48,13 @@ public class MainActivity extends Activity {
             double valoresmatriz[][] = {
 
                     new double[]{1, 0.001, 100, 1000, 1000000, 1000000000, 0.0006, 1.0936, 3.2808, 39.9701, 0.00053996},
+                    new double[]{1, 1000, 1000000, 0.001, 1e-6, 1e-9, 1e-12, 8e+6, 2e+6, 8000},
+                    new double[]{1, 0.84, 105.41, 0.75, 1.36, 8.75, 34.86, 24.66, 7.71, 21.75},
+                    new double[]{ 0.001,1,10,1000,28.3495,0.5,1000000},
+                    new double[]{ 8e+6,8000,1000,8,1,0.008,0.001,8e-6,1e-6},
+                    new double[]{ 1, 0.264172,1000,0.001,0.0353147,4.16667,67.628,33.814},
+                    new double[]{ 8.64e+7,86400,1440,24,1,0.142857,0.032876643423,0.00273973,0.000273973,2.73973e-5},
+
 
             };
 
@@ -63,6 +70,55 @@ public class MainActivity extends Activity {
                     a = SpiVal.getSelectedItemPosition();
                     resp = valoresmatriz[0][a] / valoresmatriz[0][de] * can;
                     break;
+
+                case "Almacenamiento":
+                    SpiVal = (Spinner) findViewById(R.id.spnAlmacenamientoA);
+                    de = SpiVal.getSelectedItemPosition();
+                    SpiVal = (Spinner) findViewById(R.id.spnAlmacenamientoB);
+                    a = SpiVal.getSelectedItemPosition();
+                    resp = valoresmatriz[1][a] / valoresmatriz[1][de] * can;
+                    break;
+
+                case "Moneda":
+                    SpiVal = (Spinner) findViewById(R.id.spnMonedaA);
+                    de = SpiVal.getSelectedItemPosition();
+                    SpiVal = (Spinner) findViewById(R.id.spnMonedaB);
+                    a = SpiVal.getSelectedItemPosition();
+                    resp = valoresmatriz[2][a] / valoresmatriz[2][de] * can;
+                    break;
+
+                case "Peso":
+                    SpiVal = (Spinner) findViewById(R.id.spnPesoA);
+                    de = SpiVal.getSelectedItemPosition();
+                    SpiVal = (Spinner) findViewById(R.id.spnPesoB);
+                    a = SpiVal.getSelectedItemPosition();
+                    resp = valoresmatriz[3][a] / valoresmatriz[3][de] * can;
+                    break;
+
+                case "Datos":
+                    SpiVal = (Spinner) findViewById(R.id.spnDatosA);
+                    de = SpiVal.getSelectedItemPosition();
+                    SpiVal = (Spinner) findViewById(R.id.spnDatosB);
+                    a = SpiVal.getSelectedItemPosition();
+                    resp = valoresmatriz[4][a] / valoresmatriz[4][de] * can;
+                    break;
+
+                case "Volumen":
+                    SpiVal = (Spinner) findViewById(R.id.spnVolumenA);
+                    de = SpiVal.getSelectedItemPosition();
+                    SpiVal = (Spinner) findViewById(R.id.spnVolumenB);
+                    a = SpiVal.getSelectedItemPosition();
+                    resp = valoresmatriz[5][a] / valoresmatriz[5][de] * can;
+                    break;
+
+                case "Tiempo":
+                    SpiVal = (Spinner) findViewById(R.id.spnTiempoA);
+                    de = SpiVal.getSelectedItemPosition();
+                    SpiVal = (Spinner) findViewById(R.id.spnTiempoB);
+                    a = SpiVal.getSelectedItemPosition();
+                    resp = valoresmatriz[6][a] / valoresmatriz[6][de] * can;
+                    break;
+
             }
 
             ConverVal = (TextView) findViewById(R.id.txtRespuesta);
